942102280397ca75ccdcf2ea7ea2a7865285cedb
Leopold Talirz <leopold.talirz@gmail.com>
2020-10-12 10:53:50 +0200

    add further validation checks (#13)
     * validate that CURATED-COFs ID is unique
     * validate the "name" is unique (to decide whether we want this - we
    may well decide that it is fine. For the moment it gives an indication of
    which COFs to double-check, though)
    * check that duplicate marking is reciprocal

--- .github/validate.py
+++ .github/validate.py
@@ -34,6 +34,59 @@ def validate_unique_dois():
 
     print('No duplicate DOIs found.')
 
+@cli.command('unique-cof-ids')
+def validate_unique_cof_ids():
+    """Check that CURATED-COF IDs are unique."""
+    ids = FRAMEWORKS_DF['CURATED-COFs ID'].str.lower()
+
+    duplicates = [item for item, count in collections.Counter(list(ids)).items() if count > 1]
+
+    if duplicates:
+        print('Error: Duplicate CURATED-COF IDs detected: {}'.format(duplicates))
+        sys.exit(1)
+
+    print('No duplicate CURATED-COF IDs found.')
+
+
+@cli.command('unique-cof-names')
+def validate_unique_cof_names():
+    """Check that CURATED-COF names are unique."""
+    names = FRAMEWORKS_DF['Name'].str.lower()
+    names = names.str.replace('-',' ')
+
+    duplicates = [item for item, count in collections.Counter(list(names)).items() if count > 1]
+
+    if duplicates:
+        print('Warning: Duplicate CURATED-COF names detected: {}'.format(duplicates))
+        sys.exit(1)
+
+    print('No duplicate CURATED-COF names found.')
+
+@cli.command('duplicates-marked-reciprocally')
+def duplicates_marked_reciprocally():
+    """Check that marking of rows with "Duplicate found" is reciprocal."""
+    ids = FRAMEWORKS_DF['CURATED-COFs ID'].str
+    messages = []
+
+    for _index, row in FRAMEWORKS_DF.iterrows():
+        if row['Duplicate found'] != 'none':
+            original_id = row['CURATED-COFs ID']
+            duplicate_id = row['Duplicate found']
+            duplicate_row = FRAMEWORKS_DF.loc[FRAMEWORKS_DF['CURATED-COFs ID'] == duplicate_id ]
+            if not len(duplicate_row) == 1:
+                messages.append(f'Found row without reciprocal duplicate mark:\n{row}')
+
+            duplicate_row_original_id = duplicate_row['Duplicate found'].values[0]
+            if not duplicate_row['Duplicate found'].values[0] == original_id:
+                messages.append(f'Duplicate row lists ID {duplicate_row_original_id}, expected {original_id}')
+
+    if messages:
+       print(messages)
+       sys.exit(1)
+
+    print('Rows marked as duplicates go both ways.')
+
+
 
 if __name__ == '__main__':
     cli()  # pylint: disable=no-value-for-parameter
